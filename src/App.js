dd6d32d7aff9370f762770058ad8207590d550c7
unknown <ycc525@nyu.edu>
2019-10-30 17:56:09 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -3,35 +3,75 @@ import './App.css';
 
 let shoppingCartItems = [
   {
-    item: 'War and Peace',
-    image: 'https://images4.penguinrandomhouse.com/cover/9781400079988',
-    description: 'War and Peace broadly focuses on Napoleon’s invasion of Russia in 1812 and follows three of the most well-known characters in literature',
-    price: 100
+    item: 'Buzz Lightyear SpaceSuit ',
+    image: 'https://bit.ly/2BUwyF7',
+    description: 'If you want to fly to Infinity and Beyond, you definitely need this spacesuit. ',
+    price: '$99.99',
+  },
+  {
+    item: 'Ice Cream Sandwich ',
+    image: 'https://bit.ly/2pqLp7o',
+    description: ' The lastest product from Haaaagen Dazs! Twelve flavor to choose! Exclusive on Mars!!! ',
+    price: '$2.99',
+  },{
+    item: 'Foodini 3D Printer for Food ',
+    image: 'https://bit.ly/2Jzl9yO',
+    description: 'Wanna make food yourself on Mars? With Foodini you can make food from chocolate to STEAK.',
+    price: '$199.99',
+  },{
+    item: 'Anpanman SUV For Kids',
+    image: 'https://bit.ly/2qW8YWj',
+    description: 'This SUV car is designed exclusively align babies that does not float in the air. ',
+    price: '$99.99',
+  },{
+    item: 'Monocle for Single-Eyed Alien ',
+    image: 'https://bit.ly/2BOG0tF',
+    description: 'Short-sighted but have no way to find a pair of glass for you? This is your best chioce.',
+    price: '$59.99',
+  },{
+    item: 'Lays Potato Chips (Classic Flavor)',
+    image: 'https://bit.ly/2NkAg08',
+    description: 'Worry about little chip floating around?  This exclusive design for Mars solves the problem.',
+    price: '$0.99',
+  },{
+    item: 'Beed Bald Head Polish',
+    image: 'https://bit.ly/2BUBji0',
+    description: 'Bald head polish made from Manuka honey. 100% Organic. Bring you a day of brightness.',
+    price: '$19.99',
   },
 ]
+
+
 
 function App() {
   return (
     <div className="App">
+      <img id="icon" src="https://bit.ly/36fM9wR"/>
       <h1>Your Shopping Cart</h1>
      <SearchForm />
-     <ShoppingCart />
+     {shoppingCartItems.map(ShoppingCart)}
+     {/* this line map the value of objects to the next function */}
     </div>
   );
 }
-
-function ShoppingCart() {
+//the component inside ShoppingCart which is Product
+function ShoppingCart(data) {
   return(
     <div className="cart">
-      <Product />
+      <Product item={data.item} image={data.image} description={data.description} price={data.price} />
     </div>
   );
 }
-
-function Product() {
+//the format for each item in the list
+function Product(props) {
   return(
     <div className="product">
-      <img src={shoppingCartItems[0].image} width="100"/>
+      <img id="img" src={props.image} width="100"/>
+      <div className="description">
+      <h3>{props.item}</h3>
+      <p>{props.description}</p>
+      
+      <p id="price">{props.price}</p></div>
     </div>
   )
 }
